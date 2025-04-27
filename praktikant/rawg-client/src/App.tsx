11e969372279bbfd434e644440af604d727f5b8f
Daniel Wirenfeldt <wirenfeldtd@gmail.com>
2025-04-27 16:39:48 +0200

    section-1-dom-border-ver

--- praktikant/rawg-client/src/App.tsx
+++ praktikant/rawg-client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, Show, Image, Box, Heading } from "@chakra-ui/react";
+import { Grid, GridItem, Show, Image, Box, Heading, Flex } from "@chakra-ui/react";
 
 function App() {
   return (
@@ -9,32 +9,56 @@ function App() {
       }}
     >
 
-<GridItem>
-<Box boxSize='sm'>
-  <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov' />
-</Box>
-</GridItem>
+      <GridItem>
+        <Box width="100%" minHeight="90vh"
+          border="2px solid red">
+          <Flex
+            mt="80vh"
+            justify="center"
+            align="center"
+            gap="10px" // optional, to add some space between them
+          >
+            <Box
+              width="40px"
+              minHeight="40px"
+              border="2px solid red"
+            />
+            <Box
+              width="40px"
+              minHeight="40px"
+              border="2px solid red"
+            />
+
+            <Box
+              width="40px"
+              minHeight="40px"
+              border="2px solid red"
+            />
+          </Flex>
+
+        </Box>
+      </GridItem>
 
 
-<GridItem>
-<Box boxSize='sm'>
-  <Heading as="h2">Daniel Wirenfeldt</Heading>
-  <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov' />
-</Box>
-</GridItem>
+      <GridItem>
+        <Box boxSize='sm'>
+          <Heading as="h2">Daniel Wirenfeldt</Heading>
+          <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov' />
+        </Box>
+      </GridItem>
 
 
-<GridItem>
-  <Box>
-    <Heading as="h3">
-      Projekt
-    </Heading>
-  </Box>
+      <GridItem>
+        <Box>
+          <Heading as="h3">
+            Projekt
+          </Heading>
+        </Box>
 
-  <Box>
-    <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov' />
-  </Box>
-</GridItem>
+        <Box>
+          <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov' />
+        </Box>
+      </GridItem>
 
     </Grid>
   );
