ccacc1993539077b519f5ebf147ad35b807342d1
Daniel Wirenfeldt <wirenfeldtd@gmail.com>
2025-04-27 15:41:29 +0200

    placeholder-dom-structure

--- praktikant/rawg-client/src/App.tsx
+++ praktikant/rawg-client/src/App.tsx
@@ -1,28 +1,41 @@
-import { Grid, GridItem, Show, Image, Box } from "@chakra-ui/react";
+import { Grid, GridItem, Show, Image, Box, Heading } from "@chakra-ui/react";
 
 function App() {
   return (
     <Grid
       templateAreas={{
-        base: `"header" "main"`,
-        lg: `"header header" "aside main"`,
+        base: `"main"`,
+        lg: `"main"`,
       }}
     >
 
+<GridItem>
 <Box boxSize='sm'>
   <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov' />
 </Box>
-      <GridItem pl="2" bg="orange.300" area={"header"}>
-        Header
-      </GridItem>
-      <Show above="lg">
-        <GridItem pl="2" bg="pink.300" area={"aside"}>
-          aside
-        </GridItem>
-      </Show>
-      <GridItem pl="2" bg="green.300" area={"main"}>
-        Main
-      </GridItem>
+</GridItem>
+
+
+<GridItem>
+<Box boxSize='sm'>
+  <Heading as="h2">Daniel Wirenfeldt</Heading>
+  <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov' />
+</Box>
+</GridItem>
+
+
+<GridItem>
+  <Box>
+    <Heading as="h3">
+      Projekt
+    </Heading>
+  </Box>
+
+  <Box>
+    <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov' />
+  </Box>
+</GridItem>
+
     </Grid>
   );
 }
